b864602c77d7e43456040f7f6bb1689a9a11166d
roja-arpith <rojaarpith.123@gmail.com>
2025-03-18 16:10:21 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, World. This PES1UG22CS484\n";
+  cout << "Hello, Jenkins. This PES1UG22CS484\n";
   cout << "I have successfully built and run this  \n";
+  cout << "PES1UG22CS484\n";
   return 0;
 }
